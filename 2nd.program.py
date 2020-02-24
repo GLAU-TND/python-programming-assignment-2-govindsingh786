3766adb129923e38564f6ba51a8ba5bb615d8425
govindsingh786 <60250464+govindsingh786@users.noreply.github.com>
2020-02-24 16:52:46 +0530

    done

---
+++ 2nd.program.py
@@ -0,0 +1,17 @@
+lst1=input()
+lst.split('')
+a=len(lst1)
+l2=[]
+for in range(len(lst1)):
+    ifi==0:
+        l2.append(min(lst1))
+        lst1.remove(min(lst1))
+    elif i==a-1:
+        l2.append(lst1[-])
+    else:
+        for jin lst1:
+            fork in lst1:
+                if l2[-1][-]==j[0] and j[-1][-1]==k[0]:
+                    l2.append(j)
+                    lst1.remove(j)
+print(l2)
