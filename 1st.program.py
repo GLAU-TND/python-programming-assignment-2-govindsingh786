a650cc8b623884eea75d47d7445e44b682899b45
govindsingh786 <60250464+govindsingh786@users.noreply.github.com>
2020-02-24 16:52:04 +0530

    done

---
+++ 1st.program.py
@@ -0,0 +1,20 @@
+a=eval(input())
+d=len(str(max(lst1)))
+c=0
+d=''
+e=[]
+print(a)
+a=str(i for i in a)
+for k in range(len(a)):
+    for i in a:
+        for j in a:
+            if i!=j and i not in e:
+                f=i+j
+                f=int(y[:d])
+                if f>c:
+                    c=f
+                    n=str(i)
+    e.append(n)
+for i in e:
+    d+=i
+print(d)
